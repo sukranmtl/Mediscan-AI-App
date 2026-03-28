851b7bd0b274565cf1a3a0078c8cdbdf584e241c
sukranmtl <sukranhappy12@gmail.com>
2026-03-28 17:26:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
 
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+        `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${apiKey}`,
         {
           method: 'POST',
           headers: {
